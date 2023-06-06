d625b86fff380189bf0e3c58f38f603ce66468de
nicor528 <nicolasriquelme528@gmail.com>
2023-06-06 11:22:22 -0300

    make it better

--- apiTwilio.js
+++ apiTwilio.js
@@ -3,13 +3,16 @@ const { setSMSCode } = require('./apiFirebase');
 
 const accountSid = 'ACf0a8deab1ddad940a0b81912e50a5064';
 const authToken = '71bdf84843e2dd39a1e53d05cc17b32a';
-const client = twilio(accountSid, authToken);
+//const client = twilio(accountSid, authToken);
 
 function createCode (phone, id) {
     const code = Math.floor(100000 + Math.random() * 900000);
     return(
         new Promise ((res, rej) => {
-            client.messages
+            setSMSCode(id, 123456).then(user => {
+                res(user)
+            }).catch(error =>  {rej(erro),console.log(error)})
+{ /*           client.messages
                 .create({
                     body: `Tu código de verificación es: ${code}`,
                     from: '+12543555883',
@@ -24,7 +27,7 @@ function createCode (phone, id) {
                 .catch(error => {
                     console.error('Error al enviar el SMS:', error)
                     rej(error)
-                });
+                });*/}
         })
     )
 }
