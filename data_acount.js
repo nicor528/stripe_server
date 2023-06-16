f78460a1912fd165350d0e7afbd5def5896f074a
KingTiger <KingTiger@gmail.com>
2023-06-16 22:21:54 +0900

    update_end

--- data_acount.js
+++
@@ -1,8 +0,0 @@
-const stripe = require('stripe')('sk_test_51MtZkaFB53J3KRhjTwuAmH3YXskxuPUOGfEijzED8POeec98XSkmfQEtYAk1Qz4By099ACfxvMY8lP2EnM6ws8IY00iNIiO20d');
-
-stripe.accounts.retrieve(
-  'acct_1MvjFh2VrUtWocHH'
-).then(acount => {
-    console.log(acount)
-})
-
--- desactivate_paylink.js
+++
@@ -1,8 +0,0 @@
-const stripe = require('stripe')('sk_test_51MtZkaFB53J3KRhjTwuAmH3YXskxuPUOGfEijzED8POeec98XSkmfQEtYAk1Qz4By099ACfxvMY8lP2EnM6ws8IY00iNIiO20d');
-
-stripe.paymentLinks.update(
-    'plink_1Mtus6FB53J3KRhjiOy743fR',
-    {active: false}
-  ).then(link => {
-    console.log("El link fue desactivado")
-})
